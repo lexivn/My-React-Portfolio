8f681b0fc8f08b2cef2c361cc840ff73dc233f64
Alecks <msae26@hotmail.com>
2024-05-16 05:40:03 -0400

    Updated

--- src/sections/floating-nav/FloatingNav.jsx
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -1,19 +1,10 @@
 import data from './data';
 import Nav from 'react-bootstrap/Nav';
-// import Scrollspy from 'react-scrollspy';
 import Navbar from './Navbar';
 import './floating-nav.css'
 
 const FloatingNav = () => {
   return (
-    // <ul id='floating__nav'>
-    //   <Scrollspy className='scrollspy' items={['header', 'about', 'services', 'portfolio', 'contact']}>
-    //     {
-    //       data.map(item => <Nav key={item.id} item={item}/>)
-    //     }
-    //   </Scrollspy>
-    // </ul>
-
     <ul id='floating__nav'>
       <Nav className='navbar' >
         {
--- src/sections/floating-nav/data.js
+++ src/sections/floating-nav/data.js
@@ -7,7 +7,7 @@ import { AiFillMessage } from "react-icons/ai";
 const data = [
   {id: 1, link: '#', icon: <AiFillHome/>},
   {id: 2, link: '#about', icon: <BsPersonFill/>},
-  {id: 3, link: '#services', icon: <MdHomeRepairService/>},
+  // {id: 3, link: '#services', icon: <MdHomeRepairService/>},
   {id: 4, link: '#portfolio', icon: <AiFillAppstore/>},
   {id: 5, link: '#contact', icon: <AiFillMessage/>}
 ]
