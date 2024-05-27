6c50f0b9359bd07a78a10910c32fda50b51e40d3
Alecks <msae26@hotmail.com>
2024-05-27 06:25:51 -0400

    Updated about

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,9 +1,9 @@
-import AboutImage from '../../assets/about.png'
-import CV from '../../assets/cv.pdf'
-import { HiDownload } from 'react-icons/hi'
-import data from './data'
-import Card from '../../components/Card'
-import './about.css'
+import AboutImage from "../../assets/about.png";
+import CV from "../../assets/cv.pdf";
+import { HiDownload } from "react-icons/hi";
+import data from "./data";
+import Card from "../../components/Card";
+import "./about.css";
 
 const About = () => {
   return (
@@ -17,36 +17,49 @@ const About = () => {
         <div className="about__right">
           <h2>About Me</h2>
           <div className="about__cards">
-            {
-              data.map(item => (
-                <Card key={item.id} className="about__card">
-                  <span className='about__card-icon'>{item.icon}</span>
-                  <h5>{item.title}</h5>
-                  <small>{item.desc}</small>
-                </Card>
-              ))
-            }
+            {data.map((item) => (
+              <Card key={item.id} className="about__card">
+                <span className="about__card-icon">{item.icon}</span>
+                <h5>{item.title}</h5>
+                <small>{item.desc}</small>
+              </Card>
+            ))}
           </div>
           <p>
-            My name is Alex Soto, passionate about web development, electronics and technologies.
+            My name is Alex Soto, passionate about web development, electronics
+            and Technology.
           </p>
           <p>
-          With a background in Electronic Engineering, 12+ years of experience in Information Technology, Networking and 1+ years of experience dedicated to the research and development of electronic devices for traffic control. My passion for programming was born at a very young age programming microcontrollers for my electronic projects, with the passing of time I saw the birth of IOT and I was impressed with all that can be created combining electronics and web development. 
+            With a background in Electronic Engineering, IT, Networking and
+            Electronics. My passion for programming was born at a very young age
+            programming microcontrollers for my electronic projects, with the
+            passing of time I saw the birth of IOT and I was impressed with all
+            that can be created combining electronics and web development.
           </p>
           <p>
-          To turn my passion for programming into my daily work, I completed the Full-Stack Web Developer program at Career Foundry (Berlin, Germany), learning JavaScript, Node.js, Express, React, Angular NoSQL and much more.
+            To turn my passion for programming into my daily work, I completed
+            the Full-Stack Web Developer program at Career Foundry (Berlin,
+            Germany), learning JavaScript, Bootstrap, React.js, Angular.js, Node.js,
+            Express.js, MongoDB, PostgreSQL and much more.
           </p>
           <p>
-          Originally from Peru, I was fortunate to live and work for many years in Italy as a Project Engineer and gain valuable experience in back-end development using C++ and C#. Now I am proud to live in Jacksonville, in the beautiful sunshine state of Florida, USA.
+            Originally from Peru, I was fortunate to live and work for many
+            years in Italy as a Project Engineer and gain valuable experience in
+            back-end development using C++ and C#. Now I am proud to live in
+            Jacksonville, in the beautiful sunshine state of Florida, USA.
           </p>
           <p>
-          Currently, I'm on the lookout for a role that values continuous learning and allows me to make meaningful contributions.
-          </p>          
-          <a href={CV} download className='btn primary'>Download CV <HiDownload /></a>
+            Seeking to contribute to a dynamic team and build scalable,
+            user-centric solutions that enhance user experience and drive
+            business success.
+          </p>
+          <a href={CV} download className="btn primary">
+            Download CV <HiDownload />
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
